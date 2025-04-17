94ced23bee107ce18e4b82ff4185df29bffc58f9
Blake Rayvid <brayvid@users.noreply.github.com>
2025-04-17 18:11:48 -0400

    Move to PostgreSQL DB

---
+++ migrations/20250417215835_create_politicians.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('politicians', table => {
+    table.increments('politician_id').primary();
+    table.string('name').notNullable();
+    table.string('position').notNullable();
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+  });
+};
+  
+  /**
+   * @param { import("knex").Knex } knex
+   * @returns { Promise<void> }
+   */
+  exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('politicians');
+  };
+  
