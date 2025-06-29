4c27df6533d87b68404d38609c3e046dad592c8c
brayvid <brayvid@users.noreply.github.com>
2025-06-29 16:25:17 -0400

    Fix seo refactor deploy

--- knexfile.cjs
+++ knexfile.cjs
@@ -1,19 +1,34 @@
-// Copyright 2024-2025 soap.fyi <https://soap.fyi>
+// knexfile.cjs
 
+// This line allows us to use the .env file to load the database URL
 require('dotenv').config();
 
+// We need to export the configuration using the older `module.exports` syntax
+// because this file is being read by the Knex command-line tool.
 module.exports = {
+
   development: {
+    client: 'pg', // PostgreSQL client
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './migrations'
+    }
+  },
+
+  production: {
     client: 'pg',
     connection: {
       connectionString: process.env.DATABASE_URL,
-      ssl: { rejectUnauthorized: false }
+      // Railway requires SSL for its PostgreSQL connections
+      ssl: { rejectUnauthorized: false } 
     },
     migrations: {
       directory: './migrations'
     },
-    seeds: {
-      directory: './seeds'
+    pool: {
+      min: 2,
+      max: 10
     }
   }
+
 };
