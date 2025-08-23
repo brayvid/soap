79faee610600fb34b91ca14b085ea7ba1b732b05
Blake Rayvid <brayvid@users.noreply.github.com>
2025-08-23 15:19:27 -0400

    www version 301

--- packages/web/next.config.ts
+++ packages/web/next.config.ts
@@ -1,6 +1,22 @@
-// packages/web/next.config.js
+// packages/web/next.config.ts
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-};
 
-module.exports = nextConfig;
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.use.soap.fyi',
+          },
+        ],
+        destination: 'https://use.soap.fyi/:path*',
+        permanent: true, // This sets the status code to 301
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig
